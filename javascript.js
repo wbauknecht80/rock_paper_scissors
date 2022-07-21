e2c360a80868110eb6ba6a4cef16caee291c6097
wbauknecht80 <wbauknecht80@gmail.com>
2022-07-21 13:57:03 -0500

    Have all fail-safes & first to five in place. Maybe I'll make it pretty, maybe I won't, but the JS portion is working correctly.

--- javascript.js
+++ javascript.js
@@ -23,32 +23,59 @@ function playRound () {
 
     if (computerSelection == 'Rock' && playerSelection == 'Rock'){
         alert("The Computer played Rock. It's a draw!");
+        return 'D';
     } else if (computerSelection == 'Paper' && playerSelection == 'Rock') {
         alert("The Computer played Paper. You lose!");
+        return 'L';
     } else if (computerSelection == 'Scissors' && playerSelection == 'Rock') {
         alert("The Computer played Scissors. You win!");
+        return 'W';
 
 //player plays paper
 
     } else if (computerSelection == 'Rock' && playerSelection == 'Paper') {
         alert("The Computer played Rock. You Win!");
+        return 'W';
     } else if (computerSelection =='Paper' && playerSelection =='Paper') {
         alert("The Computer played Paper. It's a draw!");
+        return 'D';
     } else if (computerSelection =='Scissors' && playerSelection =='Paper') {
         alert('The Computer played Scissors. You lose!');
+        return 'L';
 
 //player plays scissors
 
     } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
         alert("The Computer played Rock. You lose!");
+        return 'L';
     } else if (computerSelection == 'Paper' && playerSelection == "Scissors") {
         alert("The Computer played Paper. You win!");
+        return 'W';
     } else if (computerSelection == 'Scissors' && playerSelection =='Scissors') {
         alert("The Computer played Scissors. It's a draw!")
+        return 'D';
 
 //fail-safe
         
     } else {
         alert("Something went wrong.. try again!")
+    }
+}
+
+function game() {
+    var playerScore = 0
+    var computerScore = 0
+
+    while (playerScore < 5 && computerScore < 5) {
+        gameResult = playRound()
+        if (gameResult == 'W') {
+            playerScore += 1
+        } else if (gameResult == 'L') {
+            computerScore += 1
+    }}
+    if (playerScore == 5) {
+        alert('You won five games, congratulations!')
+    } else if (computerScore == 5) {
+        alert('The computer won five games, too bad!')
     }
 }
