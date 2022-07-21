5b6089634fc0645fe00ca9ae5e87f80b6b57703d
wbauknecht80 <wbauknecht80@gmail.com>
2022-07-21 13:33:02 -0500

    Have win, loss, and draw conditions in place. This includes a fail safe if the function fails to recognize the users inpupt. Need to implement a caps fail-safe.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,52 @@
-function getComputerChoice () {
-    let randomNumber = Math.floor(Math.random()*100);
-    var cpuNum = randomNumber % 3
-    if (cpuNum == 0){
-        var cpuChoice = "Rock"
-    } else if (cpuNum == 1){
-        var cpuChoice = "Paper"
-    } else if (cpuNum == 2){
-        var cpuChoice = "Scissors"
+function playRound () {
+    const playerSelection = prompt("Rock, Paper, or Scissors?")
+
+//RNG determines the choice of the CPU 
+    
+    function getComputerChoice() {
+        let randomNumber = Math.floor(Math.random()*100);
+        var cpuNum = randomNumber % 3
+        if (cpuNum == 0){;
+            var cpuChoice = "Rock";
+        } else if (cpuNum == 1){
+            var cpuChoice = "Paper"
+        } else if (cpuNum == 2){;
+            var cpuChoice = "Scissors"
+        }
+        return cpuChoice
     }
-    console.log(cpuChoice)
+    const computerSelection = getComputerChoice()
+    
+//player plays rock
+
+    if (computerSelection == 'Rock' && playerSelection == 'Rock'){
+        alert("The Computer played Rock. It's a draw!");
+    } else if (computerSelection == 'Paper' && playerSelection == 'Rock') {
+        alert("The Computer played Paper. You lose!");
+    } else if (computerSelection == 'Scissors' && playerSelection == 'Rock') {
+        alert("The Computer played Scissors. You win!");
+
+//player plays paper
+
+    } else if (computerSelection == 'Rock' && playerSelection == 'Paper') {
+        alert("The Computer played Rock. You Win!");
+    } else if (computerSelection =='Paper' && playerSelection =='Paper') {
+        alert("The Computer played Paper. It's a draw!");
+    } else if (computerSelection =='Scissors' && playerSelection =='Paper') {
+        alert('The Computer played Scissors. You lose!');
+
+//player plays scissors
+
+    } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
+        alert("The Computer played Rock. You lose!");
+    } else if (computerSelection == 'Paper' && playerSelection == "Scissors") {
+        alert("The Computer played Paper. You win!");
+    } else if (computerSelection == 'Scissors' && playerSelection =='Scissors') {
+        alert("The Computer played Scissors. It's a draw!")
+
+//fail-safe
+        
+    } else {
+        alert("Something went wrong.. try again!")
+    }
 }
